3ef6536bcce04c803534681ec681689c8dbbf9cd
Hubert Baumgartner <hubert@chilicode.com>
2023-04-27 11:43:26 +0200

    add DerGriueneApfel

---
+++ umiz4kidz-vorlesebuch/src/components/derGrueneApfel/der-gruene-apfel.test.jsx
@@ -0,0 +1,8 @@
+import { render, screen } from "@testing-library/react";
+import { DerGrueneApfel } from "./derGrueneApfel";
+
+describe("DerGrueneApfel", () => {
+  it("", () => {
+    render(<DerGrueneApfel />);
+  });
+});
---
+++ umiz4kidz-vorlesebuch/src/components/derGrueneApfel/derGrueneApfel.jsx
@@ -0,0 +1,8 @@
+import PropTypes from "prop-types";
+import styles from "./derGrueneApfel.module.css";
+
+export const DerGrueneApfel = (props) => {
+  return <div className={styles["der-gruene-apfel"]}></div>;
+};
+
+DerGrueneApfel.propTypes = {};
