2a317642c544e2454a5c54c18b4e651642cd32b2
Hubert Baumgartner <hubert@chilicode.com>
2023-04-27 14:53:17 +0200

    add Router and first demo

--- umiz4kidz-vorlesebuch/src/App.js
+++ umiz4kidz-vorlesebuch/src/App.js
@@ -1,7 +1,18 @@
 import logo from './logo.svg';
 import './App.css';
+import {
+  BrowserRouter as Router,
+  Routes,
+  Route,
+  Link,
+} from "react-router-dom";
+
 import DerGrueneApfel from './components/derGrueneApfel'
 import DGAPreview from './components/derGrueneApfel/1-Der gruene Apfel.jpeg'
+
+import Manatee from './components/Manatee/Manatee';
+import Narwhal from './components/Narwhal/Narwhal';
+import Whale from './components/Whale/Whale';
 
 function App() {
   return (
@@ -13,6 +24,19 @@ function App() {
               <img src={DGAPreview} />
             </a>
           </section>
+        </div>
+        <div className="wrapper">
+          <h1>Marine Mammals</h1>
+          <Router>
+            <Link to='/manatee'>manatee</Link>
+            <Link to='/narwhal'>narwhal</Link>
+            <Link to='/whale">'>whale</Link>
+            <Routes>
+              <Route path="/manatee" element={<Manatee />} />
+              <Route path="/narwhal" element={<Narwhal />} />
+              <Route path="/whale" element={<Whale />} />
+            </Routes>
+          </Router>
         </div>
       </header>
     </div>
