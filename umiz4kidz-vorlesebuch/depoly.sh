d1452baf3d1d8acc9fe03ab2cb01119755c0500e
Hubert Baumgartner <hubert@chilicode.com>
2023-06-01 08:32:03 +0200

    update deploy

--- umiz4kidz-vorlesebuch/depoly.sh
+++ umiz4kidz-vorlesebuch/depoly.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 # Connect to the server via SSH and change to the desired directory
-ssh webs 'cd /home/umiz/umiz4kidz/umiz4kidz-vorlesebuch/ && \
+ssh umiz@shodan 'whoami  && cd /var/www/umiz/umiz4kidz/umiz4kidz-vorlesebuch/ && \
   # Run git pull to update the code
   git pull && \
   # Run npm run build to build the project
