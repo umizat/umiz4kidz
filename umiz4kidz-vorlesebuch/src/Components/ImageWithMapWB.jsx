cc755ef2efe38226b90e0097ee8575105b5bd0e1
Hubert Baumgartner <hubert@chilicode.com>
2023-06-20 12:59:41 +0200

    working wörterbuch

--- umiz4kidz-vorlesebuch/src/Components/ImageWithMapWB.jsx
+++ umiz4kidz-vorlesebuch/src/Components/ImageWithMapWB.jsx
@@ -1,191 +1,93 @@
 import { useState, useEffect, useRef } from 'react';
 import { useNavigate, useParams } from 'react-router-dom';
-import { FaArrowLeft, FaArrowRight } from "react-icons/fa";
+import { FaArrowLeft, FaArrowRight } from 'react-icons/fa';
 
+const ImageWithMapWB = ({ buch }) => {
+  const { slug } = useParams();
+  const { page } = useParams();
+  const containerRef = useRef(null);
+  const navigate = useNavigate();
 
+  const [audio, setAudio] = useState(new Audio());
+  const [isPlaying, setIsPlaying] = useState(false);
 
+  const baseUrl = 'https://data.umiz4kids.at/dictionary/';
+  const pagedata = buch[page];
 
-const ImageWithMapWB = ({ vorlesebuch }) => {
+  useEffect(() => {
+    setAudio(new Audio(baseUrl + slug + '/' + page + '/39-01.mp3'));
+    setIsPlaying(false);
+  }, [slug, page]);
 
-	const { slug } = useParams();
-	const { page } = useParams();
-	const containerRef = useRef(null);
-	const navigate = useNavigate();
+  const handleIncrementPage = () => {
+    const nextPage = parseInt(page) + 1;
+    if (nextPage < Object.keys(buch).length) {
+      navigate(`/woerterbuecher/${slug}/${nextPage}`);
+    } else {
+      navigate('/woerterbuecher');
+    }
+  };
 
-	let pagedata = vorlesebuch[page];
+  const handleGoBack = () => {
+    navigate(-1);
+  };
 
-	const baseUrl = "https://data.umiz4kids.at/books/";
-
-	if (pagedata) {
-		console.log("geladen" + pagedata)
+  const handleDivClick = (index) => {
+	index = index + 1;
+	if (isPlaying) {
+	  if (audio.src.includes(`/39-0${index}.mp3`)) {
+		// Clicked the same div again, pause the audio
+		audio.pause();
+		setIsPlaying(false);
+		return;
+	  } else {
+		// Clicked a different div, pause the current audio
+		audio.pause();
+	  }
 	}
+	const newAudio = new Audio(baseUrl + slug + '/' + page + '/39-0' + index + '.mp3');
+	setAudio(newAudio);
+	setIsPlaying(true);
+	newAudio.autoplay = true;
+	console.log(`Div ${index} clicked`);
+  };
+  
 
-	/* deutsches audio */
-	const [daudio, setDaudio] = useState(null);
-	const [playingd, setPlayingd] = useState(false);
-
-	/* ungarisches audio */
-	const [uaudio, setUaudio] = useState(null);
-	const [playingu, setPlayingu] = useState(false);
-
-	/* kroatisches audio */
-	const [kaudio, setKaudio] = useState(null);
-	const [playingk, setPlayingk] = useState(false);
-
-
-	const dtoggle = () => {
-		if (playingd) {
-			daudio.pause();
-		} else {
-			if (uaudio) uaudio.pause();
-			if (kaudio) kaudio.pause();
-			daudio.play();
-		}
-		setPlayingd(!playingd);
-		setPlayingu(false);
-		setPlayingk(false);
-	};
-
-	const utoggle = () => {
-		if (playingu) {
-			uaudio.pause();
-		} else {
-			if (daudio) daudio.pause();
-			if (kaudio) kaudio.pause();
-			uaudio.play();
-		}
-		setPlayingd(false);
-		setPlayingu(!playingu);
-		setPlayingk(false);
-	};
-
-	const ktoggle = () => {
-		if (playingk) {
-			kaudio.pause();
-		} else {
-			if (daudio) daudio.pause();
-			if (uaudio) uaudio.pause();
-			kaudio.play();
-		}
-		setPlayingd(false);
-		setPlayingu(false);
-		setPlayingk(!playingk);
-	};
-
-
-	const map = vorlesebuch[page]?.['map'];
-
-	const totalPages = Object.keys(vorlesebuch).length;
-
-	const handleIncrementPage = () => {
-		daudio.pause();
-		uaudio.pause();
-		kaudio.pause();
-		const nextPage = parseInt(page) + 1;
-		if (nextPage < totalPages) {
-			navigate(`/vorlesebuecher/${slug}/${nextPage}`);
-		} else {
-			navigate('/vorlesebuecher');
-		}
-	};
-
-	const handleGoBack = () => {
-		daudio.pause();
-		uaudio.pause();
-		kaudio.pause();
-		navigate(-1);
-	};
-
-
-	useEffect(() => {
-		if (pagedata) {
-			console.log(pagedata);
-			setDaudio(new Audio(baseUrl + slug + "/" + pagedata.d));
-			setUaudio(new Audio(baseUrl + slug + "/" + pagedata.u));
-			setKaudio(new Audio(baseUrl + slug + "/" + pagedata.k));
-		}
-	}, [pagedata, slug]
-	);
-
-	useEffect(() => {
-		if (pagedata) {
-			playingd ? daudio.play() : daudio.pause();
-			playingu ? uaudio.play() : uaudio.pause();
-			playingk ? kaudio.play() : kaudio.pause();
-		}
-	}, [playingd, daudio, playingu, uaudio, playingk, kaudio]
-	);
-
-	useEffect(() => {
-		if (daudio) {
-			daudio.addEventListener('ended', () => setPlayingd(false));
-			return () => {
-				daudio.removeEventListener('ended', () => setPlayingd(false));
-			};
-		}
-		if (uaudio) {
-			uaudio.addEventListener('ended', () => setPlayingu(false));
-			return () => {
-				uaudio.removeEventListener('ended', () => setPlayingu(false));
-			};
-		}
-		if (kaudio) {
-			kaudio.addEventListener('ended', () => setPlayingk(false));
-			return () => {
-				kaudio.removeEventListener('ended', () => setPlayingk(false));
-			};
-		}
-	}, [playingd, daudio, playingu, uaudio, playingk, kaudio]);
-
-
-	return (
-		<div>
-			<nav>
-				<div ref={containerRef}>
-					{vorlesebuch && pagedata && (
-						<div className='outerImageContainer'>
-							<div className='imageContainer'>
-								<img className='' src={baseUrl + slug + "/" + pagedata.img} alt="" />
-								<div className='divWithHandCursor' onClick={dtoggle} style={{
-									position: "absolute",
-									left: `${map[0]['left']}`,
-									top: `${map[0]['top']}`,
-									width: `${map[0]["width"]}`,
-									height: `${map[0]["height"]}`,
-								}}></div>
-								<div className='divWithHandCursor' onClick={utoggle} style={{
-									position: "absolute",
-									left: `${map[1]?.left}`,
-									top: `${map[1]?.top}`,
-									width: `${map[1]?.width}`,
-									height: `${map[1]?.height}`,
-								}}></div>
-								<div className='divWithHandCursor' onClick={ktoggle} style={{
-									position: "absolute",
-									left: `${map[2]?.left}`,
-									top: `${map[2]?.top}`,
-									width: `${map[2]?.width}`,
-									height: `${map[2]?.height}`,
-								}}></div>
-							</div>
-							<div className="arrow right-arrow divWithHandCursor" onClick={handleIncrementPage}>
-								<FaArrowRight style={{ width: "100%", height: "100%" }} />
-							</div>
-							<div className="arrow left-arrow divWithHandCursor" onClick={handleGoBack}>
-								<FaArrowLeft style={{ width: "100%", height: "100%" }} />
-							</div>
-						</div>
-
-					)}
-				</div>
-			</nav>
-		</div>
-	);
-
-
-
-
-
+  return (
+    <div>
+      <nav>
+        <div ref={containerRef}>
+          {buch && pagedata && (
+            <div className="outerImageContainer">
+              <div className="imageContainer">
+                <img className="" src={baseUrl + slug + '/' + page + '/' + pagedata.img} alt="" />
+                {buch[page]?.map.map((map, index) => (
+                  <div
+                    className="divWithHandCursor"
+                    onClick={() => handleDivClick(index)}
+                    style={{
+                      position: 'absolute',
+                      left: `${map.left}`,
+                      top: `${map.top}`,
+                      width: `${map.width}`,
+                      height: `${map.height}`,
+                    }}
+                    key={index}
+                  ></div>
+                ))}
+              </div>
+              <div className="arrow right-arrow divWithHandCursor" onClick={handleIncrementPage}>
+                <FaArrowRight style={{ width: '100%', height: '100%' }} />
+              </div>
+              <div className="arrow left-arrow divWithHandCursor" onClick={handleGoBack}>
+                <FaArrowLeft style={{ width: '100%', height: '100%' }} />
+              </div>
+            </div>
+          )}
+        </div>
+      </nav>
+    </div>
+  );
 };
 
 export default ImageWithMapWB;
--- umiz4kidz-vorlesebuch/src/Pages/Woerterbuch.jsx
+++ umiz4kidz-vorlesebuch/src/Pages/Woerterbuch.jsx
@@ -1,16 +1,16 @@
 import { useState, useEffect } from 'react';
 import { useParams } from 'react-router-dom';
-import ImageWithMap from '../Components/ImageWithMap';
+import ImageWithMapWB from '../Components/ImageWithMapWB';
 
 const Woerterbuch = () => {
-  const [woerterbuch, setWoerterbuch] = useState([]);
+  const [buch, setWoerterbuch] = useState([]);
 
   const { slug } = useParams();
   const { page } = useParams();
 
   useEffect(() => {
     const fetchWoerterbuch = async () => {
-      let URL = `https://data.umiz4kids.at/books/${slug}/data.json`;
+      let URL = `https://data.umiz4kids.at/dictionary/${slug}/data.json`;
       let response = await fetch(URL);
       let data = await response.json();
       setWoerterbuch(data);
@@ -22,7 +22,7 @@ const Woerterbuch = () => {
 
   return (
     <div className="container">
-      <ImageWithMap woerterbuch={woerterbuch} />
+      <ImageWithMapWB buch={buch} />
     </div>
   );
 };
