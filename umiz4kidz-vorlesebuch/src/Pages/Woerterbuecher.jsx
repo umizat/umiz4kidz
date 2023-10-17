b5644c86feb56bb1170d5ceed193793de9208b9f
Hubert Baumgartner <hubert@chilicode.com>
2023-10-17 18:18:21 +0200

    add wbbuch-card and wbbuch-container

--- umiz4kidz-vorlesebuch/src/Pages/Woerterbuecher.jsx
+++ umiz4kidz-vorlesebuch/src/Pages/Woerterbuecher.jsx
@@ -20,9 +20,9 @@ const Woerterbuecher = () => {
 			<div className="title">
 				<h1>Wörterbücher</h1>
 			</div>
-			<div className="buecher-container">
+			<div className="wbbuecher-container">
 				{woerterbuecher.map((woerterbuech) => (
-					<div key={woerterbuech.id} className="buch-card">
+					<div key={woerterbuech.id} className="wbbuch-card">
 						<a href={`/woerterbuecher/${woerterbuech.slug}/0/`} >
 							<img src= {woerterbuech.img} alt="" className="buch-img" />
 						</a>
