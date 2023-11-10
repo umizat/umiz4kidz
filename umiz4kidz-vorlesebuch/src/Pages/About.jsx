ea033cc8513d6025beebe22b844604c2c554269a
sarah <sarah@chilicode.com>
2023-11-10 08:29:50 +0100

    foto

--- umiz4kidz-vorlesebuch/src/Pages/About.jsx
+++ umiz4kidz-vorlesebuch/src/Pages/About.jsx
@@ -10,7 +10,7 @@ const About = () => {
 			
 			
 			<div className="about-container">
-				<div>	<img src="VanderBellen.jpg" alt="Bundespräsident Alexander van der Bellen hat sich unsere Kinderbücher in Oberwart angesehen."  width="100%"/>
+				<div>	<img src="VanderBellen.jpg" alt="Bundespräsident Alexander van der Bellen hat sich unsere Kinderbücher in Oberwart angesehen."  height="100%"/>
 		</div>
 		<div>
 				<p>Unsere umiz4kids-Gruppe besteht seit 2010 und beinhaltet Themen rund ums mehrsprachige Burgenland - Veranstaltungen, Bücher, Hörbücher, Bilderbuchkinos und vieles mehr. Herzstück sind unsere deutsch - ungarisch - bgld.kroatischen Kinderbücher, die in Kindergärten, Volksschulen und im privaten Bereich Anwendung finden. 
@@ -26,7 +26,7 @@ const About = () => {
 			<img src="Bücherausstellung.jpg" alt="Bücherausstellung" width="100%" />
 
 </div>
-<div className="about-title">
+<div className="video">
 <iframe width="560" height="315" src="https://www.youtube.com/embed/mYk7-CRqvDg" frameborder="0" allowfullscreen></iframe>
 
 
