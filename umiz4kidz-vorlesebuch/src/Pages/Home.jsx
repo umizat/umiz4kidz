c8cfff6bfb32b22d3b0a377feffca957a4b8a3d2
sarah <sarah@chilicode.com>
2023-11-13 14:20:23 +0100

    slideshow

--- umiz4kidz-vorlesebuch/src/Pages/Home.jsx
+++ umiz4kidz-vorlesebuch/src/Pages/Home.jsx
@@ -3,6 +3,41 @@ import { Link } from 'react-router-dom';
 const Home = () => {
 	return (
 		<div className="container">
+
+			<div class="slideshow">
+  <img src="Mitlesebucher.jpg" alt="Mitlesebucher">
+  <img src="vokabeltrainer.jpg" alt="Vokabeltrainer">
+  <img src="vorlesebücher.jpg" alt="Vorlesebücher">
+  <img src="wörterbücher.jpg" alt="Wörterbücher">
+</div>
+
+<script>
+  // JavaScript-Code für die Slideshow
+  var currentIndex = 0;
+  var slides = document.querySelectorAll('.slideshow img');
+  var totalSlides = slides.length;
+
+  function showSlide(index) {
+    slides.forEach(function (slide) {
+      slide.style.display = 'none';
+    });
+
+    slides[index].style.display = 'block';
+  }
+
+  function nextSlide() {
+    currentIndex = (currentIndex + 1) % totalSlides;
+    showSlide(currentIndex);
+  }
+
+  // Starte die Slideshow
+  showSlide(currentIndex);
+
+  // Automatische Weiterleitung alle 3 Sekunden (3000 Millisekunden)
+  setInterval(nextSlide, 3000);
+</script>
+
+
 			<h1>Ungarisch und Burgenland-Kroatisch spielerisch kennenlernen</h1>
 			<h2>Magyar és burgenlandi-horvát nyelvvel való játékos ismerkedés</h2>
 			<h3>Igrajući upoznati madjarski i gradišćanskohrvatski jezik</h3>
