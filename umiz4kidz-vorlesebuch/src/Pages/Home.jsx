72307c736351a8d729c2b7d294721856de08a027
Hubert Baumgartner <hubert@chilicode.com>
2023-11-14 16:30:23 +0100

    added slider

--- umiz4kidz-vorlesebuch/src/Pages/Home.jsx
+++ umiz4kidz-vorlesebuch/src/Pages/Home.jsx
@@ -1,8 +1,51 @@
 import { Link } from 'react-router-dom';
+import React from "react";
+import Slider from "react-slick";
+import "slick-carousel/slick/slick.css";
+import "slick-carousel/slick/slick-theme.css";
+
+
+var settings = {
+	dots: false,
+	autoplay: true,
+	infinite: true,
+	speed: 500,
+	slidesToShow: 1,
+	slidesToScroll: 1,
+	pauseOnHover: false,
+
+};
 
 const Home = () => {
 	return (
 		<div className="container">
+
+			<Slider {...settings}>
+				<div className='slide'>
+				<a href="/vorlesebuecher">
+						<img src="vorlesebücher.jpg" alt="Vorlesebücher" />
+						<h1>Vorlesebücher</h1>
+				</a>
+				</div>
+				<div className='slide'>
+				<a href="/mitlesebuecher">
+						<img src="Mitlesebucher.jpg" alt="Mitlesebücher" />
+						<h1>Mitlesebücher</h1>
+					</a>
+				</div>
+				<div className='slide'>
+				<a href="/woerterbuecher">
+						<img src="wörterbücher.jpg" alt="Wörterbücher" />
+						<h1>Wörterbücher</h1>
+					</a>
+				</div>
+				<div className='slide'>
+				<a href="/vokabeltrainer/1">
+						<img src="vokabeltrainer.jpg" alt="Vokabeltrainer" />
+						<h1>Vokabeltrainer</h1>
+					</a>
+				</div>
+			</Slider>
 
 
 			<h1>Ungarisch und Burgenland-Kroatisch spielerisch kennenlernen</h1>
@@ -12,33 +55,33 @@ const Home = () => {
 				{/* First Link and Div */}
 				<div className="link-div">
 					<Link to="/vorlesebuecher">
-							<img src="vorlesebücher.jpg" alt="Vorlesebücher" />
-							<h1>Vorlesebücher</h1>
+						<img src="vorlesebücher.jpg" alt="Vorlesebücher" />
+						<h1>Vorlesebücher</h1>
 					</Link>
 				</div>
 				{/* Second Link and Div */}
 				<div className="link-div">
 					<Link to="/mitlesebuecher">
-							<img src="Mitlesebucher.jpg" alt="Mitlesebücher" />
-							<h1>Mitlesebücher</h1>
+						<img src="Mitlesebucher.jpg" alt="Mitlesebücher" />
+						<h1>Mitlesebücher</h1>
 					</Link>
 				</div>
 				{/* Third Link and Div */}
 				<div className="link-div">
 					<Link to="/woerterbuecher">
-							<img src="wörterbücher.jpg" alt="Wörterbücher" />
-							<h1>Wörterbücher</h1>
+						<img src="wörterbücher.jpg" alt="Wörterbücher" />
+						<h1>Wörterbücher</h1>
 					</Link>
 				</div>
 				{/* Fourth Link and Div */}
 				<div className="link-div">
 					<Link to="/vokabeltrainer/1">
-							<img src="vokabeltrainer.jpg" alt="Vokabeltrainer" />
-							<h1>Vokabeltrainer</h1>
+						<img src="vokabeltrainer.jpg" alt="Vokabeltrainer" />
+						<h1>Vokabeltrainer</h1>
 					</Link>
 				</div>
 			</div>
-		</div>
+		</div >
 	);
 };
 
