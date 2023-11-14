355aaa40ff42ab88aa3544e0f06ae38e2879ba46
sarah <sarah@chilicode.com>
2023-11-14 08:20:28 +0100

    impda

--- umiz4kidz-vorlesebuch/src/Pages/ImpDa.jsx
+++ umiz4kidz-vorlesebuch/src/Pages/ImpDa.jsx
@@ -9,20 +9,97 @@ const ImpDa = () => {
 <h1>Impressum</h1>
 
 <p>
-	Lorem ipsum dolor sit amet, consectetur adipisicing elit. Dolorem, expedita numquam, iure incidunt enim ea quia natus asperiores voluptate exercitationem voluptatum, cum perspiciatis magni ducimus laboriosam autem quas unde velit!
+Medieninhaber, Herausgeber und für den Inhalt verantwortlich: UMIZ - Ungarisches Medien- und Informationszentrum<br>
+</br>
+Adresse: A-7502 Unterwart, Kirchenallee 4<br>
+</br>
+(Postadresse abweichend: A-7502 Unterwart, Marienplatz 3)<br>
+</br>
+
+Tel: +43/3352/31287-0 <br>
+</br>
+Fax: +43/3352/31287-14<br>
+</br>
+
+E-Mail: office@umiz.at, umizmail@gmail.com<br>
+</br>
+
+FN<br>
+</br>
+UID:<br>
+</br>
+
+Handelsgericht Eisenstadt<br>
+</br>
+<br>
+</br>
+<h6>Unsere AGBs</h6>
+
+Bilder & Texte: Bilder und Texte stehen Redakteuren und Interessierten für die Berichterstattung kostenlos zur Verfügung. Bitte beachten Sie die angegebenen Copyrighthinweise.<br>
+</br>
+
+Über darüber hinausgehende Rechte für allfällige Veröffentlichungen verfügt das UMIZ - Ungarisches Medien- und Informationszentrum nicht. Diese müssen daher gegebenenfalls bei den jeweiligen Fotografen bzw. Eigentümern der Urheberrechte eingeholt werden.
+<br>
+</br>
+Alle Texte: UMIZ - Ungarisches Medien- und Informationszentrum
+<br>
+</br>
+Fotos & Videos: UMIZ - Ungarisches Medien- und Informationszentrum
+<br>
+</br>
+Links: Auf Inhalte oder Gestaltung der auf umiz4kids.at verlinkten externen Seiten haben wir keinerlei Einfluss. Diese liegen ausschließlich im Verantwortungsbereich der jeweiligen Eigentümer, inhaltlich Verantwortlichen, Betreiber oder anderer.
+<br>
+</br>
+…………………………………………………………………………………………….
+<br>
+</br>
+
+Corporate Design:© UMIZ - Ungarisches Medien- und Informationszentrum
+<br>
+</br>
+Website: Konzept, Design und Umsetzung: Chilicode OG
+<br>
+</br>
+Alle Rechte vorbehalten.
+<br>
+</br>
+
 </p>
-<p>
-	Lorem ipsum dolor sit amet, consectetur adipisicing elit. Dolorem, expedita numquam, iure incidunt enim ea quia natus asperiores voluptate exercitationem voluptatum, cum perspiciatis magni ducimus laboriosam autem quas unde velit!
-</p>
+<br></br>
+
 
 <h1>Datenschutzerklärung</h1>
 
 <p>
-	Lorem ipsum dolor sit amet, consectetur adipisicing elit. Dolorem, expedita numquam, iure incidunt enim ea quia natus asperiores voluptate exercitationem voluptatum, cum perspiciatis magni ducimus laboriosam autem quas unde velit!
+Unsere Priorität ist der Schutz Ihrer Privatsphäre im Internet. Diese Datenschutzerklärung verdeutlicht unser Engagement für den Schutz Ihrer persönlichen Daten und die sichere Verarbeitung von Informationen im Internet.
+<br></br><br></br>
+
+1. Einwilligung:
+
+Durch die Nutzung unserer Website stimmen Sie der Speicherung und Nutzung Ihrer Daten gemäß dieser Erklärung zu. Änderungen dieser Datenschutzerklärung werden auf dieser Seite veröffentlicht, um Sie über gespeicherte und genutzte Daten auf dem Laufenden zu halten.
+<br></br><br></br>
+
+
+2. Erhebung von Daten:
+
+Wenn Sie uns über ein Formular auf unserer Website kontaktieren, speichern wir Ihre angegebenen Informationen zur Bearbeitung Ihrer Anfrage für sechs Monate. Diese Daten werden nicht ohne Ihre Zustimmung weitergegeben.
+<br></br><br></br>
+
+
+3. Details zur Datenerhebung
+
+– IP-Adressen: Bei Ihrem Website-Besuch wird Ihre IP-Adresse für die Dauer Ihrer Sitzung erfasst, um technische Sicherheit zu gewährleisten.
+– Cookies: Wir verwenden Cookies für personalisierte Inhalte und zur Verbesserung der Benutzererfahrung.
+– Benutzerregistrierung: Wenn Sie sich registrieren, sammeln wir die erforderlichen Informationen.
+– E-Mail-Adressen und persönliche Daten: Die von Ihnen bereitgestellten Daten über Formulare oder E-Mail-Kontakt werden für Anfragen und Rückfragen sechs Monate lang gespeichert.
+<br></br><br></br>
+
+
+4. Auskunft und Kontakt:
+
+Wenn Sie Fragen zur Datenschutzerklärung haben oder Ihre persönlichen Daten einsehen oder aktualisieren möchten, kontaktieren Sie uns bitte.
 </p>
-<p>
-	Lorem ipsum dolor sit amet, consectetur adipisicing elit. Dolorem, expedita numquam, iure incidunt enim ea quia natus asperiores voluptate exercitationem voluptatum, cum perspiciatis magni ducimus laboriosam autem quas unde velit!
-</p>
+
 
 			
 		</div >
