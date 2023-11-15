c10cc8d4e15218077cf9a0455f435c057624b1d3
Hubert Baumgartner <hubert@chilicode.com>
2023-11-15 09:07:57 +0100

    Impressum und Datenschutz

--- umiz4kidz-vorlesebuch/src/Pages/ImpDa.jsx
+++ umiz4kidz-vorlesebuch/src/Pages/ImpDa.jsx
@@ -6,102 +6,79 @@ const ImpDa = () => {
 		<div className="container">
 
 
-<h1>Impressum</h1>
+			<div className='legal'>
 
-<p>
-Medieninhaber, Herausgeber und für den Inhalt verantwortlich: UMIZ - Ungarisches Medien- und Informationszentrum<br>
-</br>
-Adresse: A-7502 Unterwart, Kirchenallee 4<br>
-</br>
-(Postadresse abweichend: A-7502 Unterwart, Marienplatz 3)<br>
-</br>
-
-Tel: +43/3352/31287-0 <br>
-</br>
-Fax: +43/3352/31287-14<br>
-</br>
-
-E-Mail: office@umiz.at, umizmail@gmail.com<br>
-</br>
-
-FN<br>
-</br>
-UID:<br>
-</br>
-
-Handelsgericht Eisenstadt<br>
-</br>
-<br>
-</br>
-<h6>Unsere AGBs</h6>
-
-Bilder & Texte: Bilder und Texte stehen Redakteuren und Interessierten für die Berichterstattung kostenlos zur Verfügung. Bitte beachten Sie die angegebenen Copyrighthinweise.<br>
-</br>
-
-Über darüber hinausgehende Rechte für allfällige Veröffentlichungen verfügt das UMIZ - Ungarisches Medien- und Informationszentrum nicht. Diese müssen daher gegebenenfalls bei den jeweiligen Fotografen bzw. Eigentümern der Urheberrechte eingeholt werden.
-<br>
-</br>
-Alle Texte: UMIZ - Ungarisches Medien- und Informationszentrum
-<br>
-</br>
-Fotos & Videos: UMIZ - Ungarisches Medien- und Informationszentrum
-<br>
-</br>
-Links: Auf Inhalte oder Gestaltung der auf umiz4kids.at verlinkten externen Seiten haben wir keinerlei Einfluss. Diese liegen ausschließlich im Verantwortungsbereich der jeweiligen Eigentümer, inhaltlich Verantwortlichen, Betreiber oder anderer.
-<br>
-</br>
-…………………………………………………………………………………………….
-<br>
-</br>
-
-Corporate Design:© UMIZ - Ungarisches Medien- und Informationszentrum
-<br>
-</br>
-Website: Konzept, Design und Umsetzung: Chilicode OG
-<br>
-</br>
-Alle Rechte vorbehalten.
-<br>
-</br>
-
-</p>
-<br></br>
+				<h1>Impressum</h1>
 
 
-<h1>Datenschutzerklärung</h1>
 
-<p>
-Unsere Priorität ist der Schutz Ihrer Privatsphäre im Internet. Diese Datenschutzerklärung verdeutlicht unser Engagement für den Schutz Ihrer persönlichen Daten und die sichere Verarbeitung von Informationen im Internet.
-<br></br><br></br>
+				<p>
+					Medieninhaber, Herausgeber und für den Inhalt verantwortlich: UMIZ - Ungarisches Medien- und Informationszentrum<br>
+					</br>
+					Adresse: A-7502 Unterwart, Kirchenallee 4<br>
+					</br>
+					(Postadresse abweichend: A-7502 Unterwart, Marienplatz 3)<br>
+					</br>
 
-1. Einwilligung:
+					Tel: +43/3352/31287-0 <br>
+					</br>
+					Fax: +43/3352/31287-14<br>
+					</br>
 
-Durch die Nutzung unserer Website stimmen Sie der Speicherung und Nutzung Ihrer Daten gemäß dieser Erklärung zu. Änderungen dieser Datenschutzerklärung werden auf dieser Seite veröffentlicht, um Sie über gespeicherte und genutzte Daten auf dem Laufenden zu halten.
-<br></br><br></br>
-
-
-2. Erhebung von Daten:
-
-Wenn Sie uns über ein Formular auf unserer Website kontaktieren, speichern wir Ihre angegebenen Informationen zur Bearbeitung Ihrer Anfrage für sechs Monate. Diese Daten werden nicht ohne Ihre Zustimmung weitergegeben.
-<br></br><br></br>
-
-
-3. Details zur Datenerhebung
-
-– IP-Adressen: Bei Ihrem Website-Besuch wird Ihre IP-Adresse für die Dauer Ihrer Sitzung erfasst, um technische Sicherheit zu gewährleisten.
-– Cookies: Wir verwenden Cookies für personalisierte Inhalte und zur Verbesserung der Benutzererfahrung.
-– Benutzerregistrierung: Wenn Sie sich registrieren, sammeln wir die erforderlichen Informationen.
-– E-Mail-Adressen und persönliche Daten: Die von Ihnen bereitgestellten Daten über Formulare oder E-Mail-Kontakt werden für Anfragen und Rückfragen sechs Monate lang gespeichert.
-<br></br><br></br>
+					E-Mail: <a href='mailto:office@umiz.at'>office@umiz.at</a>, <a href='mailto:umizmail@gmail.com'>umizmail@gmail.com</a><br>
+					</br>
 
 
-4. Auskunft und Kontakt:
+					UID:
+				</p>
 
-Wenn Sie Fragen zur Datenschutzerklärung haben oder Ihre persönlichen Daten einsehen oder aktualisieren möchten, kontaktieren Sie uns bitte.
-</p>
+				<h6>Unsere AGBs</h6>
+				<p>
+					Bilder & Texte: Bilder und Texte stehen Redakteuren und Interessierten für die Berichterstattung kostenlos zur Verfügung. Bitte beachten Sie die angegebenen Copyrighthinweise.<br>
+					</br>
 
+					Über darüber hinausgehende Rechte für allfällige Veröffentlichungen verfügt das UMIZ - Ungarisches Medien- und Informationszentrum nicht. Diese müssen daher gegebenenfalls bei den jeweiligen Fotografen bzw. Eigentümern der Urheberrechte eingeholt werden.
+					<br>
+					</br>
+					Alle Texte: UMIZ - Ungarisches Medien- und Informationszentrum
+					<br>
+					</br>
+					Fotos & Videos: UMIZ - Ungarisches Medien- und Informationszentrum
+					<br>
+					</br>
+					Links: Auf Inhalte oder Gestaltung der auf umiz4kids.at verlinkten externen Seiten haben wir keinerlei Einfluss. Diese liegen ausschließlich im Verantwortungsbereich der jeweiligen Eigentümer, inhaltlich Verantwortlichen, Betreiber oder anderer.
+					<hr></hr>
+					Corporate Design:© UMIZ - Ungarisches Medien- und Informationszentrum
+					<br>
+					</br>
+					Website: Konzept, Design und Umsetzung: Chilicode OG
+					<br>
+					</br>
+					Alle Rechte vorbehalten.
+				</p>
 
-			
+				<h1>Datenschutzerklärung</h1>
+
+				<p>
+					Diese Datenschutzerklärung informiert Sie über die Verarbeitung Ihrer personenbezogenen Daten bei der Nutzung unserer Website, die keine Cookies verwendet, keine Daten speichert und keine Analytics-Dienste einsetzt.
+				</p>
+				<h4>Verantwortlicher</h4>
+				<p>
+
+					Verantwortlich für die Verarbeitung Ihrer personenbezogenen Daten auf dieser Website ist das UMIZ.
+				</p>
+				<h4>Keine Verwendung von Cookies</h4>
+				<p>
+
+					Diese Website verwendet keine Cookies. Es werden keine Informationen auf Ihrem Endgerät gespeichert.
+				</p>
+				<h4>Datenspeicherung</h4>
+				<p>IP-Adressen: Bei Ihrem Website-Besuch wird Ihre IP-Adresse für die Dauer Ihrer Sitzung erfasst, um technische Sicherheit zu gewährleisten.</p>
+				<h4>Keine Verwendung von Analytics-Diensten</h4>
+				<p>
+					Diese Website setzt keine Analytics-Dienste ein. Es werden keine Informationen über Ihr Nutzungsverhalten auf der Website gesammelt, analysiert oder gespeichert.
+				</p>
+			</div >
 		</div >
 
 	);
