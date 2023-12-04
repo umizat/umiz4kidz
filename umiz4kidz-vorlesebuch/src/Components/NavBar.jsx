c5123a767289ccc4166e723358e7c40389e6def1
Hubert Baumgartner <hubert@chilicode.com>
2023-12-04 12:45:08 +0100

    add burger

--- umiz4kidz-vorlesebuch/src/Components/NavBar.jsx
+++ umiz4kidz-vorlesebuch/src/Components/NavBar.jsx
@@ -1,43 +1,48 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { NavLink } from 'react-router-dom';
 
 const NavBar = () => {
-	return (
-		<div>
-			<nav>
-				<div className="nav-items container">
+  const [isMenuOpen, setIsMenuOpen] = useState(false);
 
-					<div className="logo">
+  const toggleMenu = () => {
+    setIsMenuOpen(!isMenuOpen);
+  };
 
-
-
-						<a href="/" className="zimu-class">
-							<img src="https://umiz4kids.at/szimu%20klein.jpg" alt="Zimu" /><h4>UMIZ 4 KIDS</h4>
-						</a>
-					</div>
-					<ul>
-						<li>
-							<a href="/about">Das Projekt</a>
-						</li>
-						<li>
-							<a href="/vorlesebuecher">
-								Vorlesebücher
-							</a>
-						</li>
-						<li>
-							<a href="/mitlesebuecher">Mitlesebücher</a>
-						</li>
-						<li>
-							<a href="/woerterbuecher">Wörterbücher</a>
-						</li>
-						<li>
-							<a href="/vokabeltrainer/1">Vokabeltrainer</a>
-						</li>
-					</ul>
-				</div>
-			</nav>
-		</div>
-	);
+  return (
+    <div>
+      <nav>
+        <div className={`nav-items container ${isMenuOpen ? 'menu-open' : ''}`}>
+          <div className="logo">
+            <a href="/" className="zimu-class">
+              <img src="https://umiz4kids.at/szimu%20klein.jpg" alt="Zimu" />
+              <h4>UMIZ 4 KIDS</h4>
+            </a>
+          </div>
+          <div className="burger-icon" onClick={toggleMenu}>
+            {/* You can use an SVG icon or a Unicode character for the burger icon */}
+            &#9776;
+          </div>
+          <ul className={isMenuOpen ? 'menu-open' : ''}>
+            <li>
+              <a href="/about">Das Projekt</a>
+            </li>
+            <li>
+              <a href="/vorlesebuecher">Vorlesebücher</a>
+            </li>
+            <li>
+              <a href="/mitlesebuecher">Mitlesebücher</a>
+            </li>
+            <li>
+              <a href="/woerterbuecher">Wörterbücher</a>
+            </li>
+            <li>
+              <a href="/vokabeltrainer/1">Vokabeltrainer</a>
+            </li>
+          </ul>
+        </div>
+      </nav>
+    </div>
+  );
 };
 
 export default NavBar;
